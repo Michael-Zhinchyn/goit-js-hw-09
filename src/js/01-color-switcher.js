3a1c9914f796710e002f2deb3051163af6ae3487
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-06-06 23:54:27 -0500

    update task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
 
+let commonStyles = {
+  width: '200px',
+  height: '100px',
+  borderRadius: '10px',
+  borderColor: 'transparent',
+  fontSize: '30px',
+};
+
+Object.assign(toggleSwitch.style, commonStyles);
+
+let timerId;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+toggleSwitch.addEventListener('change', () => {
+  if (toggleSwitch.checked) {
+    // Цей код виконається, коли чекбокс відмічено
+    // Тут виконується код, який був у "кліку" на кнопці 1
+    timerId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+      toggleSwitch.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  } else {
+    // Цей код виконається, коли чекбокс не відмічено
+    // Тут виконується код, який був у "кліку" на кнопці 2
+    toggleSwitch.style.backgroundColor = getRandomHexColor();
+    clearInterval(timerId);
+  }
+});
