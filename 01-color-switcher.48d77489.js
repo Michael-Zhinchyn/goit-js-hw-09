d7d0a54156586418756c03f3e89bbfd07e45eee5
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-06-07 04:54:48 +0000

    Deploying to gh-pages from @ Michael-Zhinchyn/goit-js-hw-09@3a1c9914f796710e002f2deb3051163af6ae3487 🚀

---
+++ 01-color-switcher.48d77489.js
@@ -0,0 +1,2 @@
+const t=document.querySelector('.switch input[type="checkbox"]');let e;function o(){return`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}Object.assign(t.style,{width:"200px",height:"100px",borderRadius:"10px",borderColor:"transparent",fontSize:"30px"}),t.addEventListener("change",(()=>{t.checked?e=setInterval((()=>{document.body.style.backgroundColor=o(),t.style.backgroundColor=o()}),1e3):(t.style.backgroundColor=o(),clearInterval(e))}));
+//# sourceMappingURL=01-color-switcher.48d77489.js.map
