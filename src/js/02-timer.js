8ad4c810d69a56a957de3c1f2472ee6d16257944
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-06-08 01:12:59 -0500

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,20 +3,69 @@ import flatpickr from "flatpickr";
 // Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
 
-const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
-
-
 const startBtn = document.querySelector('button[data-start]')
-startBtn.disabled = true
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    countdown(selectedDates[0]);
+  },
+};
+
+
+function countdown(targetDate) {
+  let currentDateInMs = options.defaultDate.getTime()
+  let targetDateInMs = targetDate.getTime()
+
+  if(targetDateInMs < currentDateInMs) {
+
+    window.alert('Please choose a date in the future');
+    return
+  }
+
+  let timeDiff = targetDateInMs - currentDateInMs
+
+  function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
+
+  let timerDays = document.querySelector('[data-days]').textContent
+  
+ 
+  
+  console.log(convertMs(timeDiff)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+  
+
+  
+  
+}
+
+
+
+
 
 flatpickr('input#datetime-picker', options)
 
-console.log(options.defaultDate);
+
+
+
+
+
