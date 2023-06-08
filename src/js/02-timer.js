9c629a3a952cbae403d96c87275039dd6a4043a5
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-06-08 02:24:49 -0500

    update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,9 +1,8 @@
-// Описаний в документації
 import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
 
-const startBtn = document.querySelector('button[data-start]')
+const startBtn = document.querySelector('button[data-start]');
+let targetDate = null;
 
 const options = {
   enableTime: true,
@@ -11,61 +10,54 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    countdown(selectedDates[0]);
+    targetDate = selectedDates[0];
+    if (targetDate.getTime() < options.defaultDate.getTime()) {
+      window.alert('Please choose a date in the future');
+    }
   },
 };
 
+flatpickr('input#datetime-picker', options);
 
-function countdown(targetDate) {
-  let currentDateInMs = options.defaultDate.getTime()
-  let targetDateInMs = targetDate.getTime()
+// Get the elements
+const daysSpan = document.querySelector('[data-days]');
+const hoursSpan = document.querySelector('[data-hours]');
+const minutesSpan = document.querySelector('[data-minutes]');
+const secondsSpan = document.querySelector('[data-seconds]');
 
-  if(targetDateInMs < currentDateInMs) {
+startBtn.addEventListener('click', () => {
+  if (targetDate) {
+    let countdown = setInterval(() => {
+      let currentDateInMs = new Date().getTime();
+      let timeDiff = targetDate.getTime() - currentDateInMs;
 
-    window.alert('Please choose a date in the future');
-    return
+      if(timeDiff <= 0) {
+        clearInterval(countdown);
+        console.log('Countdown Finished');
+        return;
+      } 
+
+      let remainingTime = convertMs(timeDiff);
+
+      // Update the DOM
+      daysSpan.textContent = String(remainingTime.days).padStart(2, '0');
+      hoursSpan.textContent = String(remainingTime.hours).padStart(2, '0');
+      minutesSpan.textContent = String(remainingTime.minutes).padStart(2, '0');
+      secondsSpan.textContent = String(remainingTime.seconds).padStart(2, '0');
+    },1000);
   }
+})
 
-  let timeDiff = targetDateInMs - currentDateInMs
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-  function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-  let timerDays = document.querySelector('[data-days]').textContent
-  
- 
-  
-  console.log(convertMs(timeDiff)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  
-
-  
-  
+  return { days, hours, minutes, seconds };
 }
-
-
-
-
-
-flatpickr('input#datetime-picker', options)
-
-
-
-
-
-
