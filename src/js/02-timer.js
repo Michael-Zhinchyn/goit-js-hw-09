70951aead196371829cc2c3c1bf8442775932719
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-06-09 02:53:58 -0500

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,19 +1,15 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
-const alertSound = document.querySelector('#alertSound')
-alertSound.volume = 0.3
+const daysSpan = document.querySelector('[data-days]');
+const hoursSpan = document.querySelector('[data-hours]');
+const minutesSpan = document.querySelector('[data-minutes]');
+const secondsSpan = document.querySelector('[data-seconds]');
+const alertSound = document.querySelector('#alertSound');
+alertSound.volume = 0.3;
 
-
-// Знаходимо кнопку старту
 const startBtn = document.querySelector('button[data-start]');
-// знаходжу кнопку для ресету
-const resetBtn = document.querySelector('button[data-reset]')
-
-
-// створюємо змінну для запису обраної дати
-let targetDate = null;
 
 const options = {
   enableTime: true,
@@ -23,59 +19,58 @@ const options = {
   onClose(selectedDates) {
     targetDate = selectedDates[0];
     if (targetDate.getTime() < options.defaultDate.getTime()) {
-      alertSound.play()
-      Notiflix.Report.warning('Упс ! Обрану дату з\'їли динозаври...',
-      'Будь ласка, оберіть дату з майбутнього',
-      'Гаразд')
-      startBtn.classList.remove("valid-date");
-      startBtn.classList.add("invalid-date");
+      alertSound.play();
+      Notiflix.Report.warning(
+        "Упс ! Обрану дату з'їли динозаври...",
+        'Будь ласка, оберіть дату з майбутнього',
+        'Гаразд'
+      );
+      startBtn.classList.remove('valid-date');
+      startBtn.classList.add('invalid-date');
     } else {
-      startBtn.classList.remove("invalid-date");
-      startBtn.classList.add("valid-date");
+      startBtn.classList.remove('invalid-date');
+      startBtn.classList.add('valid-date');
     }
   },
 };
 
 flatpickr('input#datetime-picker', options);
 
-// Отримуємо елементи з розмітки
-const daysSpan = document.querySelector('[data-days]');
-const hoursSpan = document.querySelector('[data-hours]');
-const minutesSpan = document.querySelector('[data-minutes]');
-const secondsSpan = document.querySelector('[data-seconds]');
+
+let targetDate = null;
+let timer = null;
+const interval = 1000
 
 
 startBtn.addEventListener('click', () => {
-  
   if (targetDate) {
-   
-    const timer = setInterval(() => {
-      
+    timer = setInterval(() => {
       let currentDateInMs = new Date().getTime();
-      
+
       let timeDiff = targetDate.getTime() - currentDateInMs;
 
-      
-      if(timeDiff <= 0) {
+      if (timeDiff <= 0) {
         clearInterval(timer);
-        
         return;
-      } 
+      }
 
-
-      // Присвоюємо змінній залишковий час
       let remainingTime = convertMs(timeDiff);
 
-      // Оновлюємо DOM
-      daysSpan.textContent = String(remainingTime.days).padStart(2, '0');
-      hoursSpan.textContent = String(remainingTime.hours).padStart(2, '0');
-      minutesSpan.textContent = String(remainingTime.minutes).padStart(2, '0');
-      secondsSpan.textContent = String(remainingTime.seconds).padStart(2, '0');
-    },1000);
-
-    
+      // // Оновлюємо DOM
+      const timeUnits = {
+        days: daysSpan,
+        hours: hoursSpan,
+        minutes: minutesSpan,
+        seconds: secondsSpan,
+      };
+      
+      Object.keys(timeUnits).forEach((unit) => {
+        timeUnits[unit].textContent = String(remainingTime[unit]).padStart(2, '0');
+      });
+    }, interval);
   }
-})
+});
+
 
 
 // функція конвертора мілісекунд в секунди, хвилини, години, дні.
@@ -112,43 +107,57 @@ function convertMs(ms) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // // ------------------------ BUTTON  VISUAL --------------------------
 const createSVG = (width, height, radius) => {
-  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 
   const rectangle = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "rect"
+    'http://www.w3.org/2000/svg',
+    'rect'
   );
 
   svg.setAttributeNS(
-    "http://www.w3.org/2000/svg",
-    "viewBox",
+    'http://www.w3.org/2000/svg',
+    'viewBox',
     `0 0 ${width} ${height}`
   );
 
-  rectangle.setAttribute("x", "0");
-  rectangle.setAttribute("y", "0");
-  rectangle.setAttribute("width", "100%");
-  rectangle.setAttribute("height", "100%");
-  rectangle.setAttribute("rx", `${radius}`);
-  rectangle.setAttribute("ry", `${radius}`);
-  rectangle.setAttribute("pathLength", "10");
+  rectangle.setAttribute('x', '0');
+  rectangle.setAttribute('y', '0');
+  rectangle.setAttribute('width', '100%');
+  rectangle.setAttribute('height', '100%');
+  rectangle.setAttribute('rx', `${radius}`);
+  rectangle.setAttribute('ry', `${radius}`);
+  rectangle.setAttribute('pathLength', '10');
 
   svg.appendChild(rectangle);
 
   return svg;
 };
 
-document.querySelectorAll(".sketch-button").forEach((button) => {
+document.querySelectorAll('.sketch-button').forEach(button => {
   const style = getComputedStyle(button);
 
-  const lines = document.createElement("div");
+  const lines = document.createElement('div');
 
-  lines.classList.add("lines");
+  lines.classList.add('lines');
 
-  const groupTop = document.createElement("div");
-  const groupBottom = document.createElement("div");
+  const groupTop = document.createElement('div');
+  const groupBottom = document.createElement('div');
 
   const svg = createSVG(
     button.offsetWidth,
@@ -171,11 +180,11 @@ document.querySelectorAll(".sketch-button").forEach((button) => {
 
   button.appendChild(lines);
 
-  button.addEventListener("pointerenter", () => {
-    button.classList.add("start");
+  button.addEventListener('pointerenter', () => {
+    button.classList.add('start');
   });
 
-  svg.addEventListener("animationend", () => {
-    button.classList.remove("start");
+  svg.addEventListener('animationend', () => {
+    button.classList.remove('start');
   });
 });
